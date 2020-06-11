0df097009525deb24f6bd24c72cb6b325c3e5ea4
Joon Park <aisgandy@gmail.com>
2020-06-11 15:31:35 +0900

    Push wind direction data to weather app

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -21,7 +21,9 @@ const forecast = (latitude, longitude, callback) => {
 					body.current.temperature +
 					' degrees out. It feels like ' +
 					body.current.feelslike +
-					' degrees out.'
+					' degrees out.' +
+					'The wind direction is ' +
+					body.current.wind_dir
 			);
 		}
 	});
